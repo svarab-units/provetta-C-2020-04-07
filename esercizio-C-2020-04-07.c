6c3140fb93925750d7eb1d954dd9b8d60764ac04
utente <utente@debian10.units.it>
2020-04-03 03:04:27 +0200

    Ex 10

--- esercizio-C-2020-04-07.c
+++ esercizio-C-2020-04-07.c
@@ -1,5 +1,6 @@
 /** ex 1 */
 #include <stdio.h>
+// #include <stdlib.h>
 #include <string.h>
 
 char * str_1 = "prima-";
@@ -16,4 +17,22 @@ int main(int argc, char **argv) {
 	strcat(str_new, str_3);
 	// Output
 	printf(str_new);
+}
+
+/** ex 10 */
+#include <stdio.h>
+#include <string.h>
+
+int my_strlen3(char * data1, char * data2, char * data3) {
+	return (data1 == NULL ? 0 : strlen(data1))
+			+ (data2 == NULL ? 0 : strlen(data2))
+			+ (data3 == NULL ? 0 : strlen(data3));
+}
+
+int main() {
+	// tests
+	printf("Len1 : %d\n", my_strlen3("0", "ab", "cde"));
+	printf("Len2 : %d\n", my_strlen3("0", NULL, "cde"));
+	printf("Len3 : %d\n", my_strlen3("0-----", "ab", NULL));
+	return 0;
 }
